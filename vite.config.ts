c70adda1999bef4035b0bd48dbdc4d0484546693
Saurabh Kumar Pandey <saurabh.skp1999@gmail.com>
2026-09-07 00:03:50 +0530

    update url

--- vite.config.ts
+++ vite.config.ts
@@ -154,7 +154,7 @@ const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(
 
 export default defineConfig({
   plugins,
-  base: process.env.GITHUB_PAGES ? '/research_portfolio/' : '/',
+  base: process.env.GITHUB_PAGES ? '/portfolio/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
